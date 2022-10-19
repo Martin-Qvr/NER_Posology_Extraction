cd6a47a80d0273b640a2d279a1ff1cd61f22f540
Sarah Mayer <mayersarah.ms@gmail.com>
2022-10-19 22:21:23 +0200

    pas de paraphrase pour les mots labelisés

--- src/paraphrase_generator.py
+++ src/paraphrase_generator.py
@@ -51,7 +51,7 @@ def get_wordvector_similarity(nlp,replacements):
     return replacements_refined
 
 
-def paraphrase_generator(phrase):
+def paraphrase_generator(phrase, dataset, i):
     augmented_data = {}
     tokenized_phrase = tokenize.sent_tokenize(phrase, language='french')
     for current_sentence in tokenized_phrase:
@@ -59,46 +59,48 @@ def paraphrase_generator(phrase):
         doc = nlp(current_sentence)
         replacements = {}
         for token in doc: 
-            if ('NOUN' in token.tag_):
-                if (token.ent_type == 0): # if its a noun and not a NER
-                    """Augment the noun with possible synonyms from Wordnet"""            
-                    syns = wordnet.synsets(token.text,'n', lang=lang)
-                    synonyms = set()
-                    for eachSynSet in syns:
-                        for eachLemma in eachSynSet.lemmas(lang):
-                            current_word = eachLemma.name()
-                            if current_word.lower() != token.text.lower() and current_word != token.lemma_:
-                                synonyms.add(current_word.replace("_"," "))
-                    synonyms = list(synonyms)
-                    #print("\tCurrent noun word:", token.text, "(",len(synonyms),")")
-                    if len(synonyms) > 0:
-                        replacements[token.text] = synonyms
-            if 'ADJ' in token.tag_: # if its an adjective
-                """Augment the adjective with possible synonyms from Wordnet"""
-                syns = wordnet.synsets(token.text,'a', lang=lang)
-                synonyms = set()
-                for eachSynSet in syns:
-                    for eachLemma in eachSynSet.lemmas(lang):
-                        current_word = eachLemma.name()
-                        if current_word.lower() != token.text.lower() and current_word != token.lemma_:
-                            synonyms.add(current_word.replace("_"," "))
-                synonyms = list(synonyms)
-                #print("\tCurrent adjective word:", token.text, "(",len(synonyms),")")
-                if len(synonyms) > 0:
-                    replacements[token.text] = synonyms
-            #if 'VERB' in token.tag_: # if its a verb
-            #    """Augment the verb with possible synonyms from Wordnet"""
-            #    syns = wordnet.synsets(token.text,'v', lang=lang)
-            #    synonyms = set()
-            #    for eachSynSet in syns:
-            #        for eachLemma in eachSynSet.lemmas(lang):
-            #            current_word = eachLemma.name()
-            #            if current_word.lower() != token.text.lower() and current_word != token.lemma_:
-            #                synonyms.add(current_word.replace("_"," "))
-            #    synonyms = list(synonyms)
-                #print("\tCurrent verb word:", token.text, "(",len(synonyms),")")
-            #    if len(synonyms) > 0:
-            #        replacements[token.text] = synonyms
+            for j in range(len(dataset["label"].iloc[i])):
+                if token.idx not in [dataset["label"].iloc[i][j][0],dataset["label"].iloc[i][j][1]]:
+                    if ('NOUN' in token.tag_):
+                        if (token.ent_type == 0): # if its a noun and not a NER
+                            """Augment the noun with possible synonyms from Wordnet"""            
+                            syns = wordnet.synsets(token.text,'n', lang=lang)
+                            synonyms = set()
+                            for eachSynSet in syns:
+                                for eachLemma in eachSynSet.lemmas(lang):
+                                    current_word = eachLemma.name()
+                                    if current_word.lower() != token.text.lower() and current_word != token.lemma_:
+                                        synonyms.add(current_word.replace("_"," "))
+                            synonyms = list(synonyms)
+                            #print("\tCurrent noun word:", token.text, "(",len(synonyms),")")
+                            if len(synonyms) > 0:
+                                replacements[token.text] = synonyms
+                    if 'ADJ' in token.tag_: # if its an adjective
+                        """Augment the adjective with possible synonyms from Wordnet"""
+                        syns = wordnet.synsets(token.text,'a', lang=lang)
+                        synonyms = set()
+                        for eachSynSet in syns:
+                            for eachLemma in eachSynSet.lemmas(lang):
+                                current_word = eachLemma.name()
+                                if current_word.lower() != token.text.lower() and current_word != token.lemma_:
+                                    synonyms.add(current_word.replace("_"," "))
+                        synonyms = list(synonyms)
+                        #print("\tCurrent adjective word:", token.text, "(",len(synonyms),")")
+                        if len(synonyms) > 0:
+                            replacements[token.text] = synonyms
+                    #if 'VERB' in token.tag_: # if its a verb
+                    #    """Augment the verb with possible synonyms from Wordnet"""
+                    #    syns = wordnet.synsets(token.text,'v', lang=lang)
+                    #    synonyms = set()
+                    #    for eachSynSet in syns:
+                    #        for eachLemma in eachSynSet.lemmas(lang):
+                    #            current_word = eachLemma.name()
+                    #            if current_word.lower() != token.text.lower() and current_word != token.lemma_:
+                    #                synonyms.add(current_word.replace("_"," "))
+                    #    synonyms = list(synonyms)
+                        #print("\tCurrent verb word:", token.text, "(",len(synonyms),")")
+                    #    if len(synonyms) > 0:
+                    #        replacements[token.text] = synonyms
         #print("Input(before filtering):\n",sum(map(len, replacements.values())))
         replacements_refined = get_wordvector_similarity(nlp,replacements)
         #print("Output(after filtering based on similarity score):\n",sum(map(len, replacements_refined.values())))
@@ -134,7 +136,7 @@ def final_dataframe_creation(file_name):
         tokenized_phrase = tokenize.sent_tokenize(phrases[i], language='french')
         print("phrase n°", i)
         for current_sentence in tokenized_phrase:
-            paraphrase = paraphrase_generator(current_sentence)
+            paraphrase = paraphrase_generator(current_sentence, dataset_df, i)
             aux = data_paraphrase.loc[data_paraphrase["text"] == phrases[i]]
             for j in range(len(paraphrase)):
                 if str(paraphrase["Phrases"].iloc[j]) != str(paraphrase["Paraphrases"].iloc[j]):
