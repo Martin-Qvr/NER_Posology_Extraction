896cd5565eae2f327549f30fc167bd92efa2121c
Arsène Tripard <arsene.tripard@hec.edu>
2022-10-20 23:16:41 +0000

    add submission csv script to main

--- main.py
+++ main.py
@@ -292,3 +292,164 @@ confusion_matrix =  seaborn.heatmap(metrics.confusion_matrix(pred_tags, valid_ta
 confusion_matrix.savefig('confusion_matrix.png', dpi=400)
 
 vizualisation.plot_learning_curve(loss_values, validation_loss_values)
+
+
+# # ========================================
+# #          Generate submission CSV
+# # ========================================
+
+import torch
+import pandas as pd
+import yaml
+import numpy as np
+
+from utils import preprocessing
+from transformers import BertTokenizer
+from keras_preprocessing.sequence import pad_sequences
+from torch.utils.data import DataLoader, SequentialSampler, TensorDataset
+
+with open("config.yaml") as f:
+    config = yaml.safe_load(f)
+
+#########################################
+########## LOAD DATA AND MODEL ##########
+#########################################
+print("Loading data and model ...")
+    
+### INPUT HERE ###
+test_csv_filepath = ""
+submission_csv_filepath = ""
+
+test = pd.read_csv(test_csv_filepath)
+
+def testcsv_to_sentences(test_df, sentences_len=100):
+    # change type for text concat
+    test_df["TokenId"] = test_df["TokenId"].astype(str)
+    
+    # concat by sentences
+    records = pd.DataFrame()
+    records["tokens"] = list(test_df.groupby("sentence_id")["token"].apply(list).values)
+    records["tokensId"] = list(test_df.groupby("sentence_id")["TokenId"].apply(list).values)
+    
+    # split a tokens list every  elements
+    records["tokens"] = records["tokens"].apply(lambda x: [x[i*sentences_len:(i+1)*sentences_len] for i in range((len(x)//sentences_len)+1)])
+    records["tokensId"] = records["tokensId"].apply(lambda x: [x[i*sentences_len:(i+1)*sentences_len] for i in range((len(x)//sentences_len)+1)])
+    
+    # expand vertically when there are more than one sentence part
+    sentences = pd.DataFrame()
+    sentences["tokens"] = records[["tokens"]].explode("tokens")["tokens"]
+    sentences["tokensId"] = records[["tokensId"]].explode("tokensId")["tokensId"]
+
+    return sentences
+
+test_sentences = list(testcsv_to_sentences(test, 60)["tokens"])
+tokens_id = list(testcsv_to_sentences(test, 60)["tokensId"])
+
+######################################
+########## TOKENIZE AND PAD ##########
+######################################
+print("Tokenizing and padding ...")
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=False)
+
+tokenized_sentences = []
+tokenized_sentences_ids = []
+for sentence, sentence_ids in zip(test_sentences, tokens_id):
+    tokenized_sentence = []
+    tokenized_sentence_id = []
+    
+    for word, id in zip(sentence, sentence_ids):
+        tokenized_word = tokenizer.tokenize(word)
+        n_subwords = len(tokenized_word)
+
+        tokenized_sentence.extend(tokenized_word)
+        tokenized_sentence_id.extend([id] * n_subwords)
+        
+    tokenized_sentences.append(tokenized_sentence)
+    tokenized_sentences_ids.append(tokenized_sentence_id)
+
+
+all_token_ids = []
+for sentence_ids in tokenized_sentences_ids:
+    for token_id in sentence_ids:
+        all_token_ids.append(int(token_id))
+
+missing_token_ids = list(set(list(range(3557))) - set(all_token_ids))
+print("There are {} missing tokens".format(len(missing_token_ids)))
+
+MAX_LEN = config["MAX_LEN"]
+bs = config["bs"]
+
+input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_sentences],
+                          maxlen=MAX_LEN, dtype="long", value=0.0,
+                          truncating="post", padding="post")
+
+attention_mask = [[float(i != 0.0) for i in ii] for ii in input_ids]
+
+###############################
+########## TO TENSOR ##########
+###############################
+print("Converting to tensors ...")
+
+test_data = torch.tensor(input_ids)
+test_masks = torch.tensor(attention_mask)
+
+test_dataset = TensorDataset(test_data, test_masks)
+test_sampler = SequentialSampler(test_dataset)
+valid_dataloader = DataLoader(test_dataset, sampler=test_sampler, batch_size=bs)
+
+#####################################
+########## MAKE PREDICTION ##########
+#####################################
+print("Making the predictions ...")
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
+model.eval()
+predictions, true_labels = [], []
+
+for batch in valid_dataloader:
+    batch = tuple(t.to(device) for t in batch)
+    b_input_ids, b_input_mask = batch
+
+    with torch.no_grad():
+        # BERT model
+        outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+        
+        # other model
+        #outputs = model(b_input_ids, attention_mask=b_input_mask)
+        
+        logits = outputs[0].detach().cpu().numpy()
+        predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
+
+########################################
+########## PREDICTIONS TO CSV ##########
+########################################
+print("Saving predictions as csv ...")
+
+new_tokens, new_labels, new_tokens_ids = [], [], []
+for sentence, sentence_predictions, sentence_tokens_ids in zip(tokenized_sentences, predictions, tokenized_sentences_ids):
+    for token, prediction, token_id in zip(sentence, sentence_predictions, sentence_tokens_ids):
+        if token.startswith("##"):
+            new_tokens[-1] = new_tokens[-1] + token[2:]
+        else:
+            new_labels.append(prediction)
+            new_tokens.append(token)
+            new_tokens_ids.append(token_id)
+
+
+predictions = pd.DataFrame([new_tokens_ids, new_tokens, new_labels]).T
+predictions = predictions.rename(columns={"0": "TokenId", "1": "Token", "2":"Predicted", 0: "TokenId", 1: "Token", 2:"Predicted"})
+predictions = predictions.drop_duplicates(subset=["TokenId"])
+predictions["TokenId"] = predictions["TokenId"].astype(int)
+
+test = pd.read_csv(test_csv_filepath)
+test["TokenId"] = test["TokenId"].astype(int)
+
+df = test.merge(predictions, how="left", on="TokenId")
+df["Predicted"] = df["Predicted"].replace(to_replace=8, value=5)
+df["Predicted"] = df["Predicted"].fillna(5).astype(int).astype(str)
+df = df[["TokenId", "Predicted"]]
+
+df.to_csv(submission_csv_filepath, index=False)
